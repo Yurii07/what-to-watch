1dbbfb16ac69814fe9c47ff51bae722dc9ebba60
Yurii Adamenko <37290705+Yurii07@users.noreply.github.com>
2022-08-01 17:08:53 +0300

    back to github

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import 'App.scss'
 
 const App: FC = () => {
   return (
-    <BrowserRouter basename='/'>
+    <BrowserRouter basename='/what-to-watch/'>
       <Provider store={store}>
         <UserAuthContextProvider>
           {navigator.onLine ? <Navigation /> : <OfflineMode />}
